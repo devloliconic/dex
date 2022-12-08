53d852a25850ebf120a2f95b9cffb0577f33fa0e
devloliconic <kirillevsmirnov@gmail.com>
2022-12-08 20:24:18 +0700

    l

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import { CartesianGrid, Line, XAxis, YAxis, LineChart } from "recharts";
 
 function App() {
   const [value, setValue] = useState(1);
-  const [a, setA] = useState();
-  const [b, setB] = useState();
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
   const [e, setE] = useState();
-  const [x, setX] = useState();
+  const [x1, setX] = useState();
+
 
   const onRadioChange = (e) => {
     setRes([]);
@@ -18,13 +19,13 @@ function App() {
 
   const getFunction = () => {
     if (value === 1) {
-      return (x) => x * x + 2 * x - 1;
+      return (x) =>  Math.pow(x,2) + 2 * x + 1;
     }
     if (value === 2) {
-      return (x) => Math.sin(x) + 1;
+      return (x) => 3-x**3
     }
     if (value === 3) {
-      return (x) => x * x - 2 * x + 1;
+      return (x) => 2*x-3;
     }
     if (value === 4) {
       return (x) => x - 1;
@@ -33,13 +34,17 @@ function App() {
 
   const func = getFunction();
 
-  const { method, res, setRes } = useResolve({ a, b, e, func });
+  const { method, res, setRes, calcCheck, checkRes,data, calcFunction } = useResolve({ a, b, e, func });
 
   const handeCalcClick = () => {
     setX(method());
+    calcFunction()
   };
-  console.log(res);
 
+  const handleClearClick = () => {
+    window.location.reload()
+  }
+  console.log(res)
   return (
     <>
       <h1 className={styled.logo}>Метод дихотомии &#128526;</h1>
@@ -52,9 +57,9 @@ function App() {
             buttonStyle="solid"
             optionType="button"
           >
-            <Radio value={1}>x^2+2x-1</Radio>
-            <Radio value={2}>sin(x)+1</Radio>
-            <Radio value={3}>x^2-2x+1</Radio>
+            <Radio value={1}>x^2+2x+1</Radio>
+            <Radio value={2}>3-x^3</Radio>
+            <Radio value={3}>2*x-3</Radio>
             <Radio value={4}>x-1</Radio>
           </Radio.Group>
           <p className={styled.text}>Задайте начальный интервал</p>
@@ -89,24 +94,50 @@ function App() {
           >
             Вычислить
           </Button>
-          {x ? <p>X = {x}</p> : null}
+          {x1 ? <p className={styled.text}>x = {x1}</p> : null}
+          {x1 ? <Button type="primary" onClick={calcCheck} style={{width: '100%'}}>Выполнить проверку</Button>: null}
+          {checkRes ? <p className={styled.text}>f(x) = {checkRes}</p>: null
+          }
+          {x1 ? <Button type="ghost" style={{width: '100%'}} onClick={handleClearClick}>Сбросить</Button> : null}
+
         </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "50px",
-        }}
+      {x1 ? (
+          <>
+            <p className={styled.text}>График получения ответа методом дихотомии</p>
+          <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
       >
         <LineChart width={600} height={300} data={res}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+          <CartesianGrid stroke="#ccc"/>
+          <XAxis dataKey="name"/>
+          <YAxis/>
         </LineChart>
       </div>
+            <p className={styled.text}>График функции</p>
+
+            <div
+        style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "20px",
+      }}
+        >
+        <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        </LineChart>
+        </div>
+          </>) : null}
     </>
   );
 }
--- src/useResolve.js
+++ src/useResolve.js
@@ -1,21 +1,46 @@
 import { useState } from "react";
 
 export const useResolve = ({ a, b, e, func }) => {
+  console.log(a, b, e, func)
+  console.log(Number(a), Number(b), Number(e))
   const [res, setRes] = useState([]);
+  const [check, setCheck] = useState(0)
+  const [checkRes, setCheckRes] = useState(0)
+  const [data, setData] = useState([])
   const method = () => {
     let c = 0;
     let a0 = Number(a);
     let b0 = Number(b);
-    while (b0 - a0 > Number(e)) {
-      c = (a0 + b0) / 2;
-      if (func(b0) * func(c) < 0) {
-        a0 = c;
-      } else {
+    let mas = [];
+    let ex = Number(e)
+    while (b0 - a0 > ex) {
+      c = (a0+b0)/2
+      if (func(a0) * func(c) < 0) {
         b0 = c;
+      } else if(func(a0) * func(c) > 0) {
+        a0 = c;
       }
-      setRes((res) => [...res, { name: a0, uv: b0 }]);
+        else {
+          setRes(mas)
+          setCheck(c)
+          return c
+      }
+      mas.push({name: a0, uv: b0})
     }
-    return (a0 + b0) / 2;
+    setRes(mas)
+    setCheck((a0+b0)/2)
+    return (a0+b0)/2;
   };
-  return { method, res, setRes };
+  const calcCheck = () => {
+    setCheckRes(func(check))
+  }
+  const calcFunction = () => {
+    const mas = []
+    for(let i = Number(a); i < Number(b); i++){
+      mas.push({name: i, uv: func(i)})
+    }
+    setData(mas)
+    return mas
+  }
+  return { method, res, setRes, calcCheck,checkRes,calcFunction, data};
 };
